f71710629adac476bcba7e80310d40ef53900747
Maicon Rodrigues <maicon.rodrigues2010mr@gmail.com>
2022-09-24 20:39:07 -0300

    Adicionado: Mostrar Lista de Pessoas cadastradas

--- Proj_ONG_ResGatinhos/Program.cs
+++ Proj_ONG_ResGatinhos/Program.cs
@@ -6,14 +6,14 @@ namespace Proj_ONG_ResGatinhos
     internal class Program
     {
         #region TELAS
-        public static void Tela_Inicial(Banco_De_Dados conn, SqlConnection conexaoSql) // OK
+        public static void Tela_Inicial(SqlConnection connection) // OK
         {
-            conexaoSql.Open();
+            connection.Open();
             do
             {
                 int opc;
                 Console.Clear();
-                Console.WriteLine("Bem Vindo à ONG ResGatinhos!");
+                Console.WriteLine("Bem Vindo à ResGatinhos!");
                 Console.WriteLine("Escolha a Opção desejada para continuar:\n");
                 Console.Write(" 1 - Acesso aos 'ADOTANTES'\n");
                 Console.Write(" 2 - Acesso aos 'PETS'\n");
@@ -26,27 +26,27 @@ namespace Proj_ONG_ResGatinhos
                     {
                         case 0:
                             Console.Clear();
-                            conexaoSql.Close();
+                            connection.Close();
                             Console.WriteLine("\n\n\n\t\t\tObrigado pela sua Cãopanhia!!!\n\t\t\tContinue com os ResGatinhos por aí!!!\n\n\n");
                             Console.WriteLine("\t\t\t......Encerrando Sessão......");
                             Environment.Exit(0);
                             break;
 
                         case 1:
-
-                            Tela_Adotantes();
+                            connection.Close();
+                            Tela_Adotantes(connection);
 
                             break;
 
                         case 2:
-
-                            Tela_Pets();
+                            connection.Close();
+                            Tela_Pets(connection);
 
                             break;
 
                         case 3:
-
-                            Tela_Adocao();
+                            connection.Close();
+                            Tela_Adocao(connection);
 
                             break;
 
@@ -61,32 +61,119 @@ namespace Proj_ONG_ResGatinhos
                 }
             } while (true);
         }
-        public static void Tela_Adotantes() // OnProgress
+        public static void Tela_Adotantes(SqlConnection connection) // OnProgress
+        {
+            connection.Open();
+            do
+            {       
+                int opc;
+                Console.Clear();
+                Console.WriteLine("ADOTANTES");
+                Console.WriteLine("O que deseja fazer?");
+                Console.WriteLine(" 1 - Ver todos Cadastrados");
+                Console.WriteLine(" 2 - Cadastrar um Novo");
+                Console.WriteLine(" 3 - Editar um Cadastro já Existente");
+                Console.WriteLine(" 4 - Ver Histórico de Adoção de um já Cadastrado");
+                try
+                {
+                    opc = int.Parse(Console.ReadLine());
+                    switch (opc)
+                    {
+                        case 0:
+                            connection.Close();
+                            Tela_Inicial(connection);
+                            break;
+
+                        case 1:
+                            connection.Close();
+                            MostrarAdotantesCadastrados(connection);
+                            break;
+
+                        case 2:
+                            connection.Close();
+
+                            break;
+
+                        case 3:
+                            connection.Close();
+
+                            break;
+
+                        case 4:
+                            connection.Close();
+
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            } while (true);
+        }
+        public static void Tela_Pets(SqlConnection connection) // OnProgress
         {
 
         }
-        public static void Tela_Pets() // OnProgress
-        {
-
-        }
-        public static void Tela_Adocao() // OnProgress
+        public static void Tela_Adocao(SqlConnection connection) // OnProgress
         {
 
         }
         #endregion
+        #region FUNCTIONS
+        static void MostrarAdotantesCadastrados(SqlConnection connection) // OK
+        {
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.CommandText = "SELECT CPF, Nome, Sexo, Data_Nascimento, Telefone, Cidade, Estado, Bairro, Rua, Numero, Complemento FROM Pessoa;";
+            cmd.Connection = connection;
+            connection.Open();
+
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                Console.Clear();
+                Console.WriteLine("\nLista de Adotantes cadastrados:");
+                Console.WriteLine("________________________________________________________________________________________________________________________________________________________________________________________________________________________________________");
+                Console.WriteLine("CPF  |   Nome  |     Sexo  |     Data de Nascimento  |   Telefone  |     Endereço");
+                Console.WriteLine("________________________________________________________________________________________________________________________________________________________________________________________________________________________________________\n\n");
+                while (reader.Read())
+                {
+                    string dataNascimento =      reader.GetDateTime(3).ToString("dd-MM-yyyy");
+                    Console.WriteLine("________________________________________________________________________________________________________________________________________________________________________________________________________________________________________");
+                    Console.Write("{0}" , /*cpf*/reader.GetString(0) + "    |    " +
+                                         /*nome*/reader.GetString(1) + "    |    " +
+                                         /*sexo*/reader.GetString(2) + "    |    " +
+                                     /*dataNasc*/dataNascimento      + "    |    " +
+                                     /*telefone*/reader.GetString(4) + "    |    " +
+                                       /*cidade*/reader.GetString(5) + "    |    " +
+                                       /*Estado*/reader.GetString(6) + "    |    " +
+                                       /*bairro*/reader.GetString(7) + "    |    " +
+                                       /*rua*/   reader.GetString(8) + "    |    " +
+                                       /*numero*/reader.GetInt32 (9) + "    |    " +
+                                  /*complemento*/reader.GetString(10)+ "    |    \n");
+                    Console.Write("________________________________________________________________________________________________________________________________________________________________________________________________________________________________________\n\n");
+                }
+                Console.WriteLine("Fim da Lista");
+                Pausa();
+                connection.Close();
+                Tela_Adotantes(connection);
+            }
+        }
         static void Pausa() // OK
         {
             Console.WriteLine("Aperte [- ENTER -] para continuar...");
             Console.ReadKey();
             Console.Clear();
         }
+        #endregion
         static void Main(string[] args)
         {
             //Conexão para o Banco de Dados:
             Banco_De_Dados conn = new Banco_De_Dados();
-            SqlConnection conexaoSql = new SqlConnection(conn.Caminho());
+            SqlConnection connection = new SqlConnection(conn.Caminho());
 
-            Tela_Inicial(conn, conexaoSql);
+            Tela_Inicial(connection);
+
         }
     }
 }
