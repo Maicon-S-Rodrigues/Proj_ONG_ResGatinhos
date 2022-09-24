4cb95e54e5942a4577c453fc21e2cb8c3944bab0
Maicon Rodrigues <maicon.rodrigues2010mr@gmail.com>
2022-09-24 14:49:45 -0300

    Adicionado: Propiedades e Construtor para Classe 'Pessoa'.

--- Proj_ONG_ResGatinhos/Pessoa.cs
+++ Proj_ONG_ResGatinhos/Pessoa.cs
@@ -8,5 +8,34 @@ namespace Proj_ONG_ResGatinhos
 {
     internal class Pessoa
     {
+        public string CPF { get; set; }
+        public string Nome { get; set; }
+        public char Sexo { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Telefone { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string Rua { get; set; }
+        public int Numero { get; set; }
+        public string Complemento { get; set; }
+
+        public Pessoa ()
+        {
+
+        }
+
+        public Pessoa(string cpf, string nome, char sexo, DateTime dataNascimento, string telefone, string estado, string cidade, string rua, int numero, string complemento)
+        {
+            CPF = cpf;
+            Nome = nome;
+            Sexo = sexo;
+            DataNascimento = dataNascimento;
+            Telefone = telefone;
+            Estado = estado;
+            Cidade = cidade;
+            Rua = rua;
+            Numero = numero;
+            Complemento = complemento;
+        }
     }
 }
