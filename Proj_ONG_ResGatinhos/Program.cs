91bfabf71657614d2bd02b2380216689ad843237
Maicon Rodrigues <maicon.rodrigues2010mr@gmail.com>
2022-09-24 15:19:06 -0300

    Adicionado: Propiedades e Construtores iniciais para funcionamento da Classe 'Animal'.

--- Proj_ONG_ResGatinhos/Program.cs
+++ Proj_ONG_ResGatinhos/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlClient;
 
 namespace Proj_ONG_ResGatinhos
 {
@@ -6,7 +7,12 @@ namespace Proj_ONG_ResGatinhos
     {
         static void Main(string[] args)
         {
-            
+            //Conexão para o Banco de Dados:
+            Banco_De_Dados conn = new Banco_De_Dados();
+            SqlConnection conexaoSql = new SqlConnection(conn.Caminho());
+            conexaoSql.Open();
+
+
 
         }
     }
