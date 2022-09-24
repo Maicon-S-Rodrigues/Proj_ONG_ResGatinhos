24c32759cef6e57de7dfe70d0076bd772c0bcb17
Maicon Rodrigues <maicon.rodrigues2010mr@gmail.com>
2022-09-24 15:10:13 -0300

    Adicionado: Propiedades e Construtores iniciais para funcionamento da Classe 'Animal'.

--- Proj_ONG_ResGatinhos/Animal.cs
+++ Proj_ONG_ResGatinhos/Animal.cs
@@ -8,5 +8,29 @@ namespace Proj_ONG_ResGatinhos
 {
     internal class Animal
     {
+        public string Familia { get; set; }
+        public string Raca { get; set; }
+        public string Nome { get; set; }
+        public char Sexo { get; set; }
+        public string Situacao { get; set; }
+
+        public Animal()
+        {
+
+        }
+        public Animal(string familia, char sexo) // para criar um novo cadastro com apenas esses dados necessarios e então editar os restantes no update ou no Insert
+        {
+            Familia = familia;
+            Sexo = sexo;
+        }
+
+        public Animal(string familia, string raca, string nome, char sexo, string situacao) // caso necessario criar um preenchido totalmente vindo de um registro do banco
+        {
+            Familia = familia;
+            Raca = raca;
+            Nome = nome;
+            Sexo = sexo;
+            Situacao = situacao;
+        }
     }
 }
