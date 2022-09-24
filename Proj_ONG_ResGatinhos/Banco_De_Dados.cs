3840a71b3fbc26b2113fed40ae5d4897009ea2bd
Maicon Rodrigues <maicon.rodrigues2010mr@gmail.com>
2022-09-24 14:33:45 -0300

    Adicionado: Classe Banco_De_Dados para definir o caminho de conexão com o Banco.

---
+++ Proj_ONG_ResGatinhos/Banco_De_Dados.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj_ONG_ResGatinhos
+{
+    internal class Banco_De_Dados
+    {
+        string Conexao = "Data Source=localhost; Initial Catalog=Resgatinhos; User ID=sa; password=Sol@2905;";
+
+        public string Caminho()
+        {
+            return Conexao;
+        }
+    }
+}
