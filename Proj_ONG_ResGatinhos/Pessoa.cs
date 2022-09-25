a3d9bbe6fcbbf5bb977b579b07b2107a85d7cc2d
Maicon Rodrigues <maicon.rodrigues2010mr@gmail.com>
2022-09-25 17:50:24 -0300

    Adicionado: LerNumeroRes() e LerData() pra facilitar cadastros. Completo: Cadastro de Adotantes

--- Proj_ONG_ResGatinhos/Pessoa.cs
+++ Proj_ONG_ResGatinhos/Pessoa.cs
@@ -10,11 +10,12 @@ namespace Proj_ONG_ResGatinhos
     {
         public string CPF { get; set; }
         public string Nome { get; set; }
-        public char Sexo { get; set; }
+        public string Sexo { get; set; }
         public DateTime DataNascimento { get; set; }
         public string Telefone { get; set; }
         public string Estado { get; set; }
         public string Cidade { get; set; }
+        public string Bairro { get; set; }
         public string Rua { get; set; }
         public int Numero { get; set; }
         public string Complemento { get; set; }
@@ -24,7 +25,7 @@ namespace Proj_ONG_ResGatinhos
 
         }
 
-        public Pessoa(string cpf, string nome, char sexo, DateTime dataNascimento, string telefone, string estado, string cidade, string rua, int numero, string complemento)
+        public Pessoa(string cpf, string nome, string sexo, DateTime dataNascimento, string telefone, string estado, string cidade, string bairro, string rua, int numero, string complemento)
         {
             CPF = cpf;
             Nome = nome;
@@ -33,6 +34,7 @@ namespace Proj_ONG_ResGatinhos
             Telefone = telefone;
             Estado = estado;
             Cidade = cidade;
+            Bairro = bairro;
             Rua = rua;
             Numero = numero;
             Complemento = complemento;
--- Proj_ONG_ResGatinhos/Program.cs
+++ Proj_ONG_ResGatinhos/Program.cs
@@ -13,8 +13,8 @@ namespace Proj_ONG_ResGatinhos
             {
                 int opc;
                 Console.Clear();
-                Console.WriteLine("Bem Vindo à ResGatinhos!");
-                Console.WriteLine("Escolha a Opção desejada para continuar:\n");
+                Console.WriteLine(" Bem Vindo à ResGatinhos!");
+                Console.WriteLine(" Escolha a Opção desejada para continuar:\n");
                 Console.Write(" 1 - Acesso aos 'ADOTANTES'\n");
                 Console.Write(" 2 - Acesso aos 'PETS'\n");
                 Console.Write(" 3 - Acesso à 'Area de ADOÇÃO'\n");
@@ -65,15 +65,16 @@ namespace Proj_ONG_ResGatinhos
         {
             connection.Open();
             do
-            {       
+            {
                 int opc;
                 Console.Clear();
-                Console.WriteLine("ADOTANTES");
-                Console.WriteLine("O que deseja fazer?");
+                Console.WriteLine("ADOTANTES\n");
+                Console.WriteLine(" O que deseja fazer?");
                 Console.WriteLine(" 1 - Ver todos Cadastrados");
                 Console.WriteLine(" 2 - Cadastrar um Novo");
                 Console.WriteLine(" 3 - Editar um Cadastro já Existente");
                 Console.WriteLine(" 4 - Ver Histórico de Adoção de um já Cadastrado");
+                Console.WriteLine(" 0 - Voltar");
                 try
                 {
                     opc = int.Parse(Console.ReadLine());
@@ -91,7 +92,7 @@ namespace Proj_ONG_ResGatinhos
 
                         case 2:
                             connection.Close();
-
+                            CadastrarNovoAdotante(connection);
                             break;
 
                         case 3:
@@ -138,19 +139,19 @@ namespace Proj_ONG_ResGatinhos
                 Console.WriteLine("________________________________________________________________________________________________________________________________________________________________________________________________________________________________________\n\n");
                 while (reader.Read())
                 {
-                    string dataNascimento =      reader.GetDateTime(3).ToString("dd-MM-yyyy");
+                    string dataNascimento = reader.GetDateTime(3).ToString("dd-MM-yyyy");
                     Console.WriteLine("________________________________________________________________________________________________________________________________________________________________________________________________________________________________________");
-                    Console.Write("{0}" , /*cpf*/reader.GetString(0) + "    |    " +
+                    Console.Write("{0}", /*cpf*/reader.GetString(0) + "    |    " +
                                          /*nome*/reader.GetString(1) + "    |    " +
                                          /*sexo*/reader.GetString(2) + "    |    " +
-                                     /*dataNasc*/dataNascimento      + "    |    " +
+                                     /*dataNasc*/dataNascimento + "    |    " +
                                      /*telefone*/reader.GetString(4) + "    |    " +
                                        /*cidade*/reader.GetString(5) + "    |    " +
                                        /*Estado*/reader.GetString(6) + "    |    " +
                                        /*bairro*/reader.GetString(7) + "    |    " +
                                        /*rua*/   reader.GetString(8) + "    |    " +
-                                       /*numero*/reader.GetInt32 (9) + "    |    " +
-                                  /*complemento*/reader.GetString(10)+ "    |    \n");
+                                       /*numero*/reader.GetInt32(9) + "    |    " +
+                                  /*complemento*/reader.GetString(10) + "    |    \n");
                     Console.Write("________________________________________________________________________________________________________________________________________________________________________________________________________________________________________\n\n");
                 }
                 Console.WriteLine("Fim da Lista");
@@ -159,11 +160,145 @@ namespace Proj_ONG_ResGatinhos
                 Tela_Adotantes(connection);
             }
         }
+        static void CadastrarNovoAdotante(SqlConnection connection) // OK
+        {
+            Console.Clear();
+            SqlCommand cmd = connection.CreateCommand();
+            cmd.CommandText = "INSERT INTO Pessoa (CPF, Nome, Sexo, Data_Nascimento, Telefone, Cidade, Estado, Bairro, Rua, Numero, Complemento)" +
+                              "VALUES (@CPF, @Nome, @Sexo, @Data_Nascimento, @Telefone, @Cidade, @Estado, @Bairro, @Rua, @Numero, @Complemento);";
+            cmd.Connection = connection;
+            connection.Open();
+
+            SqlParameter SQLcpf = new SqlParameter("@CPF", System.Data.SqlDbType.VarChar, 11);
+            SqlParameter SQLnome = new SqlParameter("@Nome", System.Data.SqlDbType.VarChar, 50);
+            SqlParameter SQLsexo = new SqlParameter("@Sexo", System.Data.SqlDbType.Char, 1);
+            SqlParameter SQLdataNasc = new SqlParameter("@Data_Nascimento", System.Data.SqlDbType.Date);
+            SqlParameter SQLtelefone = new SqlParameter("@Telefone", System.Data.SqlDbType.VarChar, 12);
+            SqlParameter SQLcidade = new SqlParameter("@Cidade", System.Data.SqlDbType.VarChar, 50);
+            SqlParameter SQLestado = new SqlParameter("@Estado", System.Data.SqlDbType.Char, 2);
+            SqlParameter SQLbairro = new SqlParameter("@bairro", System.Data.SqlDbType.VarChar, 50);
+            SqlParameter SQLrua = new SqlParameter("@Rua", System.Data.SqlDbType.VarChar, 50);
+            SqlParameter SQLnumero = new SqlParameter("@Numero", System.Data.SqlDbType.Int);
+            SqlParameter SQLcomplemento = new SqlParameter("@Complemento", System.Data.SqlDbType.VarChar, 50);
+//___________________________________________________________________________________________________________________________________________________________
+            //variaveis locais
+            string cpf, nome, sexo, telefone, cidade, estado, bairro, rua, complemento;
+            DateTime dataNasc;
+            int numero;
+
+            Console.WriteLine("CADASTRO DE ADOTANTE\n\n");
+            Console.Write("Informe o CPF para começar: ");
+            cpf = Console.ReadLine();
+
+            Console.Write("Nome: ");
+            nome = Console.ReadLine();
+
+
+            bool flag = false;
+            do
+            {
+                Console.Write("\nDigite [- F -] para Feminino, [- M -] para Masculino ou [- N -] caso Não queira informar");
+                Console.Write("\nSexo: ");
+                sexo = Console.ReadLine();
+                if (sexo.ToUpper() == "F" || sexo.ToUpper() == "M" || sexo.ToUpper() == "N")
+                {
+                    flag = true;
+                    break;
+                }
+            } while (flag == false);
+
+
+            Console.Write("\nData de Nascimento (dd/MM/yyy): ");
+            dataNasc = LerData();
+
+            Console.Write("\nTelefone: ");
+            telefone = Console.ReadLine();
+
+            Console.Write("\n\nENDEREÇO");
+            Console.Write("\nCidade: ");
+            cidade = Console.ReadLine();
+
+            Console.Write("\nEstado (U-F): ");
+            estado = Console.ReadLine();
+
+            Console.Write("\nBairro: ");
+            bairro = Console.ReadLine();
+
+            Console.Write("\nRua: ");
+            rua = Console.ReadLine();
+
+            Console.Write("\nNúmero: ");
+            numero = LerNumeroResidencial();
+
+            Console.Write("\nComplemento: ");
+            complemento = Console.ReadLine();
+
+            Pessoa P = new Pessoa(cpf, nome, sexo, dataNasc, telefone, estado, cidade, bairro, rua, numero, complemento);
+            //___________________________________________________________________________________________________________________________________________________________
+
+            SQLcpf.Value = P.CPF;
+            SQLnome.Value = P.Nome;
+            SQLsexo.Value = P.Sexo;
+            SQLdataNasc.Value = P.DataNascimento;
+            SQLtelefone.Value = P.Telefone;
+            SQLcidade.Value = P.Cidade;
+            SQLestado.Value = P.Estado;
+            SQLbairro.Value = P.Bairro;
+            SQLrua.Value = P.Rua;
+            SQLnumero.Value = P.Numero;
+            SQLcomplemento.Value = P.Complemento;
+
+            cmd.Parameters.Add(SQLcpf);
+            cmd.Parameters.Add(SQLnome);
+            cmd.Parameters.Add(SQLsexo);
+            cmd.Parameters.Add(SQLdataNasc);
+            cmd.Parameters.Add(SQLtelefone);
+            cmd.Parameters.Add(SQLcidade);
+            cmd.Parameters.Add(SQLestado);
+            cmd.Parameters.Add(SQLbairro);
+            cmd.Parameters.Add(SQLrua);
+            cmd.Parameters.Add(SQLnumero);
+            cmd.Parameters.Add(SQLcomplemento);
+
+            cmd.Prepare();
+            cmd.ExecuteNonQuery();
+            connection.Close();
+        }
         static void Pausa() // OK
         {
             Console.WriteLine("Aperte [- ENTER -] para continuar...");
             Console.ReadKey();
             Console.Clear();
+        }
+        static DateTime LerData() // OK
+        {
+            do
+            {
+                try
+                {
+                    DateTime data = DateTime.Parse(Console.ReadLine());
+                    return data;
+                }
+                catch
+                {
+                    Console.WriteLine("Data Inválida!");
+                }
+            } while (true);
+        }
+        static int LerNumeroResidencial() // OK
+        {
+            do
+            {
+                try
+                {
+                    int numero = int.Parse(Console.ReadLine());
+                    return numero;
+                }
+                catch
+                {
+                    Console.WriteLine("Número inválido!");
+                }
+            } while (true);
         }
         #endregion
         static void Main(string[] args)
